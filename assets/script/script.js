e66d62aa60e04b418cafa54e9b67ef9b12f260d7
MultiMouse <olmos.luisd@outlook.com>
2023-10-05 21:02:44 -0500

    added this class method

--- assets/script/script.js
+++ assets/script/script.js
@@ -556,3 +556,18 @@ editButton.click(function(event) {
   $("div").remove(".flip");
 });
 
+$(document).on('input', '.energy-slide', function() {
+  $('.energy-percent').html($(this).siblings().children(".energy-slide").val() + "%");
+}); //fix later
+$(".save").click(function(event) {
+  event.preventDefault();
+  var inputArray = [];
+  var dayData = $(this).parent().parent().attr("id");
+  var waterInput = $(this).siblings().children().children(".water-qty").val();
+  var stressInput = $(this).siblings().children().children(".stress-slide").val();
+  var moodInput = $(this).siblings().children().children().children("input[name=mood]:radio:checked").val();
+  var energyInput = $(this).siblings().children().children(".energy-slide").val();
+  var sleepInput = $(this).siblings().children(".hours-sleep").find(":selected").val();
+  inputArray.push(waterInput, stressInput, moodInput, energyInput, sleepInput);
+  localStorage.setItem(dayData, JSON.stringify(inputArray));
+});
